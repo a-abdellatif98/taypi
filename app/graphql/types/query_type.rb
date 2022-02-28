add33873c4e4979ef8278410731b1fa83be0e794
Ahmed M. Abd EL-latif <ahmed.abdelatife@gmail.com>
2022-02-28 14:20:24 +0200

    initial commit

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,20 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+
+    
+  field :items, 
+  [Types::ItemType],
+  null: false, 
+  description: "Return a list of items"
+  
+  def items
+    Item.all
+  end 
+  end
+end
